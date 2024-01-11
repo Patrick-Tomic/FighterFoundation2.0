d3f75eddbe420f0200fe029a187e922d61a37433
Patrick-Tomic <pat12tomic@gmail.com>
2024-01-11 01:23:11 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
+import "./index.scss";
+
+ReactDOM.createRoot(document.querySelector("body")!).render(
+  <React.StrictMode>
+     
+  </React.StrictMode>,
+);
