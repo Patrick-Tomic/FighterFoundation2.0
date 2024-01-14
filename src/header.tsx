bb64716cc6576d7791614fe46abffcbce1378757
Patrick-Tomic <pat12tomic@gmail.com>
2024-01-14 12:44:04 -0500

    content on home and style

--- src/header.tsx
+++ src/header.tsx
@@ -27,7 +27,7 @@ export default function Header(){
         <header key='header' className="p-10 bg-gray-500"  >
             <img key='logo' className='ml-24 rounded-md border-solid border-black border-2' src = {logo} alt = 'FighterFoundation'></img>
             <h1 className='text-3xl mt-3 ml-80' >"I Think there is Always Hope"<br></br>-Michelle B. Dejong</h1>
-            <h3 className='max-h-14 ml-10 border-solid border-2 border-black rounded-3xl text-2xl text-center p-1 mr-5 bg-white text-gray-500'>Fight to cure Glioblastoma!</h3>
+            <h3 className='max-h-14 ml-16 border-solid border-2 border-black rounded-3xl text-2xl text-center p-1 mr-5 bg-white text-gray-500'>Fight to cure Glioblastoma!</h3>
             <nav key='navbar' className='flex ml-44'>{tabList}</nav>
         </header>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -57,15 +57,15 @@ export default function Main(){
   const objList = objectives.map((obj) =>{
     const key = `${IDcount}obj`
     IDcount++
-    return <li className='p-3' key = {key}> {obj}</li>
+    return <li className='p-3 text-white text-xl ' key = {key}> {obj}</li>
   })
     
     return (
-       <main className='border-solid border-red-500 border-2 flex flex-col'>
+       <main className=' flex flex-col '>
         <a className="border-solid border-2 p-2  border-black" href="https://www.paypal.com/us/fundraiser/charity/2413284"><h1>Donate and Support</h1></a>
         
 
-      <div id='wrap'>
+      <div className='border-solid border-black border-3' id='wrap'>
         <div style = {style} id='slider'>
         {imgElements}
         {firstClone}
@@ -73,23 +73,30 @@ export default function Main(){
         </div>
         <div id='content' className="
         grid grid-cols-2
-        grid-row-2
-        gap-5
-        ml-36 mt-10
+        auto-rows-auto
+        gap-x-48
+        gap-y-8
+         mt-10
         ">
-          <div className="flex flex-col w-2/5 p-10 row-span-2" id='Bio'>
+          <div className="flex flex-col w-2/5 p-10 row-span-2 text-white" id='Bio'>
              <h1>Who is Michelle?</h1>
              <p className='text-l'> On March 14th, 2014 Michelle Boyd DeJong was diagnosed with an inoperable brain tumor. The tumor was located on her brain stem and its position was blocking the normal circulation of cerebral brain fluid, which resulted in pooling and increased pressure<br></br><br></br> An endoscopic ventriculostomy and biopsy was performed on march 17th, followed by the insertion of a left ventriculoperitoneal shunt on March 27th; having a subsequent revision on June 23rd. <br></br><br></br> Released from Moffitt Cancer Center the day after her shunt surgery, she made a quick recovery and promptly sneaked out to have her forearm tattooed with our "fighter" logo. The prescribed 42 days of chemotherapy and 33 radiation treatments started on April 16th.<br></br><br></br>During the week between brain surgeries Michelle realized that there was only one way to face this challenge and it was the same way she approached life: with a positive and selfless attitude focused on using her experiences to help others. The "fighter" mentality that she embodied is so much more than a battle against her brain tumor; more importantly, it is a daily initiative to inspire others through action and attitude. This begins with a personal conscious decision and ends with changing lives.</p>
                 </div>
-          <div id='whoIs'className='p-0 '><h1>Who is a Fighter?</h1><p className='text-l p-2'> A fighter does not inspire because they have cancer, but a fighter inspires because of the character with which they fight it. A fighter faces challenges with unwavering positivity and relentless hope. Hope that they may take every opportunity to make a difference in the world. They fight for love, for family, for life.</p>
+          <div id='whoIs'className='p-0 '><h1>Who is a Fighter?</h1><p className='text-xl text-white p-2'> A fighter does not inspire because they have cancer, but a fighter inspires because of the character with which they fight it. A fighter faces challenges with unwavering positivity and relentless hope. Hope that they may take every opportunity to make a difference in the world. They fight for love, for family, for life.</p>
           <h1>Fight On!</h1>
                </div>
           <div>
             <h1>Fighter Foundation Objectives</h1>
-            <ul className="list-disc p-5">
+            <ul className="list-disc p-2">
               {objList}
             </ul>
+          
                   </div>
+                  <div className='grid-cols-2' id='awareness'>
+              <h1>What is Glioblastoma?</h1>
+              <p className='text-xl text-white'>Glioblastoma(GBM) are malignant grade 4 tumors located in the brain. GBM spreads in the brain diffusely, covering large portions. GBM is able to spread to the opposite side of the brain through the corpus callosum or the ventricular system. Glioblastoma is most commonly found within the frontal lobe, statistically over 25% of the time. <br></br><br></br> GBM arise de novo, thye begin as a grade 4 tumor, with no evidence of a lower-grade precursor. The tendency of de novo tumors can be severe, and are most common in patients 60 years of age or older, younger patients may be affected </p>
+            </div>
+           
           </div>   
              
         
