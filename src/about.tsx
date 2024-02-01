b0c0149a668404273490121943824411a662fd89
Patrick-Tomic <pat12tomic@gmail.com>
2024-02-01 08:26:22 -0500

    responsiveness of about page

--- src/about.tsx
+++ src/about.tsx
@@ -76,13 +76,20 @@ export default function About(){
              <div className='grid grid-cols-3 gap-20' id='board'>
                  {boardList}
              </div>
-             <div className='flex  border-solid border-black border-2 justify-around'>
-             <div className='p-20 w-2/5'>
+             <div className='lg:flex  border-solid border-black border-2 justify-around md:block'>
+             <img className='xl:w-[50vh] xl:h-[100vh] 2xl:w-[50vh] 2xl:h-[100vh] lg:w-[38vw] lg:h-[77vh] lg:min-h-[50vh]
+             md:w-[35vw] md:h-[70vh] md:float-right md:m-5
+             sm:float-right sm:m-10
+              shadow-lg mt-24 shadow-black' src={michelle}></img>
+           
+          
+           <div className='p-10 xl:w-2/5'>
                 <h1 className='text-3xl text-center font-bold '>Who is Michelle</h1>
-                <p className='text-center text-md'>"On March 14" (title of Michelle's book), Michelle Boyd DeJong was diagnosed with an inoperable brain tumor. Because the tumor is located on her brain stem, its position was blocking the normal circulation of cerebral brain fluid, which resulted in pooling an increased pressure. An endoscopic ventriculostomy and biopsy was performed on March 17th, followed by the insertion of a left ventriculopertoneal shunt on March 27th and its subsequent revision on June 23rd.<br></br> Released from Moffit Cancer Center the day after her shunt surgery, she made a quick recovery and promptly sneaked out to have her forearm tattooed with the 'fighter' logo. The prescribed 42 days of temodar and 33 radiation treatments started on April 16th.<br></br>During the week between brain surgeries, Michelle realized that there was only one way to face this challenge, and it was the same way she approached life: with a positive and selfless attitude focused on using her experiences to help others. The "fighter" mentality that she embodies is so much more than a battle against her brain tumor. It is a daily initiative to inspire others through action and attitude. This begins with a personal conscious decision and ends with changing lives.</p>
-             </div>
-            
-            <img className=' w-[50vh] shadow-lg  w-1/5 mt-24 h-[100vh] shadow-black' src={michelle}></img>
+                <p className='text-center text-md font-semibold xl:text-xl xl:p-8 2xl:xl 2xl:p-10 lg:text-lg md:text-lg md:p-2
+                md:text-justify
+                sm:text-justify'>"On March 14" (title of Michelle's book), Michelle Boyd DeJong was diagnosed with an inoperable brain tumor. Because the tumor is located on her brain stem, its position was blocking the normal circulation of cerebral brain fluid, which resulted in pooling an increased pressure. An endoscopic ventriculostomy and biopsy was performed on March 17th, followed by the insertion of a left ventriculopertoneal shunt on March 27th and its subsequent revision on June 23rd.<br></br> Released from Moffit Cancer Center the day after her shunt surgery, she made a quick recovery and promptly sneaked out to have her forearm tattooed with the 'fighter' logo. The prescribed 42 days of temodar and 33 radiation treatments started on April 16th.<br></br>During the week between brain surgeries, Michelle realized that there was only one way to face this challenge, and it was the same way she approached life: with a positive and selfless attitude focused on using her experiences to help others. The "fighter" mentality that she embodies is so much more than a battle against her brain tumor. It is a daily initiative to inspire others through action and attitude. This begins with a personal conscious decision and ends with changing lives.</p>
+           </div>
+         
            
         </div>
             </main>
@@ -98,8 +105,8 @@ function createPerson(name: ReactElement<any, string | JSXElementConstructor<any
     return (
         <div key = {key} className='p-20'>   
             {pic}
-            <h1 className='text-2xl font-bold border-b-8 text-center border-double border-black '>{name}</h1>
-            <h3 className='text-center text-2xl max-w-52 Playfair'>{title}</h3>
+            <h1 className='text-2xl font-bold border-b-8 text-center border-double border-black lg:min-w-[200px] '>{name}</h1>
+            <h3 className='text-center text-2xl max-w-52 Playfair lg:min-w-[200px]'>{title}</h3>
         </div>
     )
 }
