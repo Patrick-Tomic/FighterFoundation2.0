93eec472a84e3242dca3949839836d8cae6e6d94
Patrick-Tomic <pat12tomic@gmail.com>
2024-02-01 07:02:36 -0500

    added content to programs

--- src/links/nav.tsx
+++ src/links/nav.tsx
@@ -10,21 +10,13 @@ export default function NavBar(){
                     
                     
                     <div id='programs'>
-                    <li className='hover:min-h-12 border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white' key ='programs'><Link to='/programs'>Programs</Link> </li>
-                     <li id ='hidden' className='border-solid border-2 text-center border-black  list-none 2xl:w-24 l h-10 te4 lt-center p-1 bg-white  xl:text-sm
-                    min-h-12 lg:w-16 lg:text-xs
-                     ' key = 'patientResources'><Link to='/'>Patient Resources</Link></li>
-                     <li id ='hidden' className='border-solid border-2 border-black  list-none 2xl:w-24 l  te4 lt-center p-1 text-center bg-white xl:text-sm
-                     min-h-12 lg:w-16 lg:text-xs
-                     ' key = 'caregiverResources'> <Link to='/'>CareGiver Resources</Link></li>
-                     <li id ='hidden' className='border-solid border-2 border-black text-center list-none 2xl:w-24 l h-10 te4 lt-center p-1  bg-white xl:text-sm
-                    min-h-12 lg:w-16 lg:text-xs 
-                     ' key = 'FCT'> <Link to='/'>Funding Clinical Trials</Link></li>
+                    <li className=' border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white' key ='programs'><Link to='/programs'>Programs</Link> </li>
+                      
                     </div>
-                    <li key = 'events' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 bg-white text-lg lg:w-16 lg:text-base'> <Link to= '/'>Events</Link></li>
-                    <li key = 'blog' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 bg-white text-lg lg:w-16 lg:text-base'> <Link to= '/'>Blog</Link></li>
-                    <li key = 'contact' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 bg-white text-lg lg:w-20 lg:text-base'> <Link to= '/'>Contact</Link></li>
-                    <li key = 'store' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 bg-white text-lg lg:text-base lg:w-16'> <Link to= '/'>Store</Link></li>
+                    <li key = 'events' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white'> <Link to= '/'>Events</Link></li>
+                    <li key = 'blog' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white'> <Link to= '/'>Blog</Link></li>
+                    <li key = 'contact' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white'> <Link to= '/'>Contact</Link></li>
+                    <li key = 'store' className='border-solid border-2 border-black list-none 2xl:w-24 h-10 text-center p-1 2xl:text-lg bg-white'> <Link to= '/'>Store</Link></li>
             </nav>
             <Outlet />
         </>
--- src/programs.tsx
+++ src/programs.tsx
@@ -19,7 +19,7 @@ const caregiver = [{
 const caregiverLinks = caregiver.map((object) => {
     return (
         <div className=' border-solid border-black border-2 bg-white gap-0  grid grid-cols-2 grid-rows-2'>
-            <img className='w-96 mt-16 bg-white' src={object.img} />
+            <img className='w-96 mt-16 ml-12 bg-white' src={object.img} />
             <a className="underline hover:text-[#FFBF00]  ml-[5vw] text-blue-500 text-4xl mt-32 " href= {object.link}>{object.linkText}</a>
             <p className='w-[100%] p-10 col-span-2 text-xl ml-[-10px]'>{object.description}</p>
         </div>
@@ -30,25 +30,30 @@ const patient = [
         link:'https://glioblastomafoundation.org/patients/glioblastoma-patient-and-caregiver-support-groups#:~:text=The%20Glioblastoma%20Support%20Circle%20is,empathetic%20understanding%2C%20and%20social%20connection',
         description:'The mission of the Glioblastoma Foundation is to transform the standard of care for glioblastoma. They support the development of new drugs and other effective therapies for glioblastoma.',
         img:GBMFoundation,
-        linkText:'Glioblastoma Patient and Caregiver Support Groups'
+        linkText:'Glioblastoma Patient and Caregiver Support Groups',
+        id:'GBM'
     },
     {
         link:'https://www.hopkinsmedicine.org/brain-tumor/patient-resources',
-        description:'John Hopkins Medicine',
-        img:JHM
+        description:'The mission of Johns Hopkins Medicine is to improve the health of the community and the world by setting the standard of excellence in medical education, research and clinical care',
+        img:JHM,
+        linkText:'Patient Resources and Support Groups',
+        id:'hopkins'
     },
     {
         link:'https://www.oligonation.org/living-with-oligo/oligodendroglioma-emotional-journey/?gclid=CjwKCAiApuCrBhAuEiwA8VJ6JrWfWS4dSmim6ZOxm--',
-        description:'Oligo Nation',
-        img:oligo
+        description:'Oligo Nation was founded to bring the oligodendroglioma community together and mobilize around funding medical research for new, more effective treatments.',
+        img:oligo,
+        linkText:'Emotional Journey',
+        id:'oligo'
     },
 ]
 const patientLinks = patient.map((object) => {
     return (
-        <div className='border-solid border-black border-2 bg-white gap-0 p-5 grid grid-cols-2'>
+        <div className='border-solid border-black border-2 bg-white gap-0 p-5 grid grid-cols-2' id={object.id}>
             
-            <img className='w-96 bg-white' src={object.img} alt={object.description} />
-            <a className="underline text-blue-500 text-3xl mt-32" href= {object.link}> {object.linkText}</a>
+            <img className='w-96 ml-12 bg-white' src={object.img} alt={object.description} />
+            <a className="underline text-blue-500 text-3xl mt-40" href= {object.link}> {object.linkText}</a>
             <p className='text-3xl col-span-2'>{object.description}</p>
         </div>
     )
